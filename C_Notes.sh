eec7d349a27d877afb9f77c8bc3cf6b0975b4bf0
fongelias <fongelias@gmail.com>
2017-08-05 11:17:14 -0400

    byte representation from chapter 2.4-6

--- C_Notes.sh
+++ C_Notes.sh
@@ -1,4 +1,11 @@
-#C files end in '.c'
-#clang is the name of the comiler
-#To compile a c file, use the following command
-make [filename].c
+# C files end in '.c'
+# clang is the name of the comiler
+# To compile a c file, use the following command
+make [filename]
+# note: do not add the extension at the end of the filename, or an error will occur
+
+
+# C does not have a boolean type
+
+# Not really C, but this is a useful cli to display an ascii table
+man ascii
